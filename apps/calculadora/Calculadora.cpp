beb28c2fac79d8a4b0b4e2b3acaea1e9aab9a82f
Cordero-GG <d.cordero.1@estudiantec.cr>
2025-09-17 15:00:17 -0600

    base

---
+++ apps/calculadora/Calculadora.cpp
@@ -0,0 +1,35 @@
+///<Summary>
+///Se define la clase de calculadora, y la lógica de los métodos
+///<Summary>
+
+#include "Calculadora.h"
+#include <iostream>
+
+Calculadora::Calculadora() = default;
+Calculadora::~Calculadora() = default;
+
+void Calculadora::operación(punteroBasico a, punteroBasico b, char símbolo)
+{
+    switch (símbolo)
+    {
+    case '+': *a += *b; break;
+    case '-': *a -= *b; break;
+    case '*': *a *= *b; break;
+    case '/':
+        if (*b != 0) *a /= *b;
+        else std::cout << "No se pueden hacer diviciones entre 0";
+        break;
+    default:
+        std::cout << "El simbolo ingresado no es válido";
+    }
+    std::cout << "El resultado es: " << *a;
+}
+
+void Calculadora::agregarMemoriaArray(punteroArray arreglo, punteroBasico elemento, std::size_t* indice)
+{
+    if (*indice < 100000) // Tamaño fijo del arreglo
+    {
+        arreglo[*indice] = *elemento;
+        (*indice)++;
+    }
+}
